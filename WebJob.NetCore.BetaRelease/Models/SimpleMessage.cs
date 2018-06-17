d62c49708a78a8bbb8efaaded529800b8a931842
Nguyen Huu Toan <nguyentoanuit@gmail.com>
2018-06-17 17:06:05 +0700

    webjob with sdk beta release

---
+++ WebJob.NetCore.BetaRelease/Models/SimpleMessage.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace WebJob.NetCore.BetaRelease.Models
+{
+    public class SimpleMessage
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("value")]
+        public string Value { get; set; }
+    }
+}
---
+++ WebJob.NetCore.BetaRelease/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob.NetCore.BetaRelease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+            {
+                config.UseDevelopmentSettings();
+            }
+
+            config.LoggerFactory = new LoggerFactory().AddConsole();
+
+            var host = new JobHost(config);
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
